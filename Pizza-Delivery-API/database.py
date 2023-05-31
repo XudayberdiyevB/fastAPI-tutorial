44c32ad3212a295ec115652b572f924350e3704f
bekzodxudayberdiyev <khuyberdievdev@gmail.com>
2023-05-31 18:33:41 +0500

    env variables added

--- Pizza-Delivery-API/database.py
+++ Pizza-Delivery-API/database.py
@@ -1,7 +1,14 @@
+from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgresql://p10:123@localhost/pizza_db', echo=True)
+env = dotenv_values(".env")
+
+DB_NAME = env.get("DB_NAME")
+DB_USER = env.get("DB_USER")
+DB_PASSWORD = env.get("DB_PASSWORD")
+SQL_ALCHEMY_DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}'
+engine = create_engine(SQL_ALCHEMY_DB_URL, echo=True)
 
 Base = declarative_base()
 
